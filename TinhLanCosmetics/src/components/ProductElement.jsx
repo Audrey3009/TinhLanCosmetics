a438f7a27b991a58afeb21412517947e368a0621
Audrey3009 <bntn737@gmail.com>
2024-05-24 17:14:16 +0700

    edit element product

--- TinhLanCosmetics/src/components/ProductElement.jsx
+++ TinhLanCosmetics/src/components/ProductElement.jsx
@@ -9,22 +9,23 @@ const ProductElement = ({ id, title, image, rating, price, brandName }) => {
   };
   return (
     <div className="max-w-2xl">
-      <div className="shadow-md rounded-lg max-w-sm bg-base-100">
+      <div className="shadow-md rounded-lg max-w-sm bg-base-100 flex flex-col h-full">
         <Link to={`/shop/product/${id}`} onClick={() => window.scrollTo(0, 0)}>
           <img
-            className="rounded-t-lg p-8"
+            className="rounded-t-lg p-8 object-cover"
             src={`https://${image}`}
             alt="product image"
           />
         </Link>
-        <div className="px-5 pb-5">
+        <div className="px-5 pb-5 flex-grow flex flex-col">
           <Link to={`/shop/product/${id}`} onClick={() => window.scrollTo(0, 0)}>
-            <h3 className="font-semibold text-base tracking-tight mb-5 text-accent-content">
+            <h3 className="font-semibold text-base tracking-tight mb-5 text-accent-content ">
               {title}
             </h3>
           </Link>
-          <div className="flex items-center justify-between">
-            <span className="text-lg font-bold text-accent-content">${price}</span>
+          <div className="flex items-center justify-between mt-auto">
+            <span className="text-lg font-bold text-accent-content" style={{ color: "#d62828" }}>${price}</span>
+            <span className="text-sm text-gray-500 line-through ml-2">$100</span>
           </div>
         </div>
       </div>
--- TinhLanCosmetics/src/components/SectionTitle.jsx
+++ TinhLanCosmetics/src/components/SectionTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SectionTitle = ({title, path}) => {
   return (
     <div className='section-title-div border-b py-5 border-gray-600'>
-        <h1 className='section-title-title text-6xl text-center mb-2 max-md:text-7xl max-sm:text-6xl text-accent-content'>{ title }</h1>
+        <h1 className='section-title-title text-6xl text-center mb-2 max-md:text-6xl max-sm:text-6xl text-accent-content'>{ title }</h1>
         <p className='section-title-path text-2xl text-center max-sm:text-xl text-accent-content'>{ path }</p>
     </div>
   )
